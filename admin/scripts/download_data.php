e7877033ddac2837bb05386537a9da64f1fa99df
fedyfausto <fedyfausto@hotmail.com>
2016-05-26 19:15:10 +0200

    Debug parser

--- admin/scripts/download_data.php
+++ admin/scripts/download_data.php
@@ -151,13 +151,15 @@ for($i = 0; $i < count($DATA_TYPES); ++$i) {
 			$file->seek($i);
 			$VALUES='"'.implode('","', explode("\t",$file->current())).'"';
 			$STRING = "INSERT INTO $CLASS_NAME ($FIELD_STRING) VALUES ($VALUES);";
-			fwrite_stream($file_quieries,$STRING);
+			$bytewrited=fwrite($file_quieries,$STRING,strlen(utf8_decode($STRING)));
+			trace($bytewrited." / ".strlen(utf8_decode($STRING)));
 			unset($VALUES);
 			unset($STRING);
 			$perc = floor(min(100,($i / $FILE_LINES)*100));
 			$CURRENT_PERCENT=$perc;
 			traceline("Parsing in corso... $perc% [$i]");
 			if($cronometro_local->passed(10)){
+
 				$client_db_system->queryDB("UPDATE Task SET percentage = $perc WHERE name = '$NAME_EXPERIMENT';");
 			}
 			break;
