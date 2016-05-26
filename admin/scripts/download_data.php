603554500a34b9b505ef8b7f63221a47c75f3359
fedyfausto <fedyfausto@hotmail.com>
2016-05-27 00:28:43 +0200

    message

--- admin/scripts/download_data.php
+++ admin/scripts/download_data.php
@@ -210,16 +210,28 @@ for($i = 0; $i < count($DATA_TYPES); ++$i) {
 			$client_db->queryDB("create class $CLASS_NAME");
 		}
 
+		$file_quieries = fopen('queries.sh', 'w');
+		fwrite($file_quieries,"
+			connect remote:localhost/$NAME_EXPERIMENT root root;
+			SET ignoreErrors TRUE;
+			SET echo FALSE;");
 
 		foreach ($FIELD_ARRAY as $key => $propriety) {
 			if($key==0){
 				#trace("Prima chiave STRINGA trovata: $propriety");
-				$client_db->queryDB( "CREATE PROPERTY $CLASS_NAME.$propriety STRING");
+				#$client_db->queryDB( "CREATE PROPERTY $CLASS_NAME.$propriety STRING");
+				fwrite($file_quieries,"CREATE PROPERTY $CLASS_NAME.$propriety STRING"."\n");
 			}
 			else{
-				$client_db->queryDB( "CREATE PROPERTY $CLASS_NAME.$propriety DOUBLE");
+				#$client_db->queryDB( "CREATE PROPERTY $CLASS_NAME.$propriety DOUBLE");
+				fwrite($file_quieries,"CREATE PROPERTY $CLASS_NAME.$propriety DOUBLE"."\n");
 			}
 		}
+		fwrite($file_quieries,"DISCONNECT;");
+		fclose($file_quieries);
+		exec("$PATH_ORIENT/console.sh $CURRENT_DIR/queries.sh");
+		unlink('queries.sh');
+
 
 		trace("Creazione della classe $CLASS_NAME completata [{$cronometro_local->stop()}]");
 
