3fbf3027d5b35fb09954d13514e1eacb90393e9a
fedyfausto <fedyfausto@hotmail.com>
2016-05-27 08:15:35 +0200

    Fixed class create

--- admin/assets/js/main.js
+++ admin/assets/js/main.js
@@ -4,4 +4,20 @@ $(document).ready(function(){
 	system.taskList(function(data){
 		console.log(data);
 	});
+
+	system.ramUsage(function(data){
+		console.log(data);
+	});
+
+	system.getBandwidth(function(data,null){
+		console.log(data);
+	});
+
+	system.getServerLoad(function(data){
+		console.log(data);
+	});
+
+	system.getDiskSpace(function(data){
+		console.log(data);
+	});
 });
